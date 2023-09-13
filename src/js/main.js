caa71b0f0252b128e6520fcff726e066615d8902
aariv <aariv.tmk@gmail.com>
2023-09-14 01:17:55 +0530

    fixed dice images on production

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,10 @@
 import "../css/style.css";
+import one from "../images/1.png";
+import two from "../images/2.png";
+import three from "../images/3.png";
+import four from "../images/4.png";
+import five from "../images/5.png";
+import six from "../images/6.png";
 
 // Import all of Bootstrap's JS
 import * as bootstrap from "bootstrap";
